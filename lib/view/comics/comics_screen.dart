d3edf48de6ec0fd519ab941bc815cec0846ef538
Gustavo <gustavotch@hotmail.com>
2022-04-13 21:45:10 -0300

    Feature: ComicsScreen UI

--- lib/view/comics/comics_screen.dart
+++ lib/view/comics/comics_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:marvel_guide/repository/comics_repository.dart';
+import 'package:marvel_guide/view/home/widgets/comic_card.dart';
 import 'package:marvel_guide/view/home/widgets/custom_progress_indicator.dart';
 
 import '../../controller/comics_controller.dart';
@@ -31,23 +32,36 @@ class _ComicsScreenState extends State<ComicsScreen> {
       appBar: AppBar(
         title: const Text('Quadrinhos'),
       ),
-      body: FutureBuilder(
-        future: _fetchComics(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.hasData) {
-            List<ComicModel> comics = snapshot.data as List<ComicModel>;
-            return ListView.builder(
-              itemCount: comics.length,
-              itemBuilder: (context, index) {
-                return Text(comics[index].title);
-              },
-            );
-          } else {
-            return const CustomProgressIndicator();
-          }
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: _fetchComics(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData) {
+              List<ComicModel> comics = snapshot.data as List<ComicModel>;
+              return GridView.count(
+                childAspectRatio: 9/16,
+                crossAxisCount: 3,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 8,
+                children: <Widget>[
+                  for (var comic in comics)
+                    ComicCard(title: comic.title, imagePath: comic.imageUrl)
+                ],
+              );
+            } else {
+              return const CustomProgressIndicator();
+            }
+          },
+        ),
       ),
     );
   }
 }
+// ListView.builder(
+            //   itemCount: comics.length,
+            //   itemBuilder: (context, index) {
+            //     return Text(comics[index].title);
+            //   },
+            // );
---
+++ lib/view/home/widgets/comic_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:marvel_guide/core/app_colors.dart';
+
+class ComicCard extends StatelessWidget {
+  final String title;
+  final String imagePath;
+
+  const ComicCard({Key? key, required this.title, required this.imagePath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black54,
+      ),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+              child: Image.network(
+            '$imagePath/portrait_xlarge.jpg',
+            fit: BoxFit.cover,
+          )),
+          Text(
+            title,
+            textAlign: TextAlign.justify,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+        ],
+      ),
+    );
+  }
+}
