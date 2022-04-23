1ef9248f459758ac7319a203a1816d01b5634092
Gustavo <gustavotch@hotmail.com>
2022-04-23 18:36:51 -0300

    Feature: HeroesList shimmer loading

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:marvel_guide/controller/home_controller.dart';
 import 'package:marvel_guide/repository/home_repository.dart';
 import 'package:marvel_guide/route/route.dart' as route;
+import 'package:marvel_guide/view/home/widgets/list_shimmer.dart';
 import 'package:marvel_guide/view/home/widgets/user_header.dart';
 import 'package:marvel_guide/view/widgets/heroes_list.dart';
 
@@ -16,7 +17,8 @@ class _HomeScreenState extends State<HomeScreen> {
   late HomeController controller;
   late ScrollController _scrollController;
 
-  final loading = ValueNotifier(true);
+  final isLoading = ValueNotifier(true);
+  bool isFirstLoading = true;
   String _username = '';
   bool _noMoreResults = false;
 
@@ -35,7 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   _fetchHeroes() async {
     if (!_noMoreResults) {
-      loading.value = true;
+      isLoading.value = true;
       if (!await controller.fetchHeroes()) {
         const snackBar = SnackBar(
           content: Text('Não há mais resultados'),
@@ -45,7 +47,13 @@ class _HomeScreenState extends State<HomeScreen> {
           _noMoreResults = true;
         });
       }
-      loading.value = false;
+      isLoading.value = false;
+
+      if (isFirstLoading) {
+        setState(() {
+          isFirstLoading = false;
+        });
+      }
     }
   }
 
@@ -76,15 +84,20 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            UserHeader(username: _username, logout: _handleLogout),
-            Flexible(
-              child: HeroesList(
-                animation: controller,
-                scrollController: _scrollController,
-                loading: loading,
-                heroes: controller.heroes,
-              ),
+            UserHeader(
+              username: _username,
+              logout: _handleLogout,
             ),
+            isFirstLoading
+                ? const ShimmerList()
+                : Flexible(
+                    child: HeroesList(
+                      animation: controller,
+                      scrollController: _scrollController,
+                      loading: isLoading,
+                      heroes: controller.heroes,
+                    ),
+                  ),
           ],
         ),
       ),
---
+++ lib/view/home/widgets/list_shimmer.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:marvel_guide/core/app_colors.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerList extends StatelessWidget {
+  const ShimmerList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) => Shimmer.fromColors(
+          highlightColor: AppColors.red,
+          baseColor: AppColors.darkRed,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+              width: double.infinity,
+              height: 100,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
